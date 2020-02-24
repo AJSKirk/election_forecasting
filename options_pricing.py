b0402537385f5f1a4ac8a49eec70ddcfbbc41183
Andrew Kirk <ajskirk@gmail.com>
2020-02-24 22:24:47 +0000

    Correct BS formula to use percentage volatility

--- options_pricing.py
+++ options_pricing.py
@@ -7,6 +7,6 @@ def option_price(value_today, payout_threshold, volatility, time_rem):
     return 1 - stats.norm.cdf((payout_threshold - value_today) / (volatility * math.sqrt(time_rem)))
 
 
-def black_scholes_price(value_today, payout_threshold, volatility, time_rem):
-    d1 = (math.log(value_today / payout_threshold) + (volatility ** 2 * time_rem / 2)) / (volatility * math.sqrt(time_rem))
-    return stats.norm.cdf(d1 - volatility * math.sqrt(time_rem))
+def black_scholes_price(value_today, payout_threshold, pct_volatility, time_rem):
+    d1 = (math.log(value_today / payout_threshold) + (pct_volatility ** 2 * time_rem / 2)) / (pct_volatility * math.sqrt(time_rem))
+    return stats.norm.cdf(d1 - pct_volatility * math.sqrt(time_rem))
